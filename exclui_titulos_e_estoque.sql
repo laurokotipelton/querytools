69ba8dcef652d15cf6e7c34f7a414b0c7c083b2c
Lauro dos Santos <laurokotipelton@gmail.com>
2017-10-31 18:37:20 -0300

    Inclusão de script para titulos

---
+++ exclui_titulos_e_estoque.sql
@@ -0,0 +1,11 @@
+--Excluir titulos e estoque
+
+delete from movimentacao_titulo_receber ;
+delete from titulo_a_receber ;
+delete from conta_a_receber ;
+
+delete from movimentacao_titulo_pagar ;
+delete from titulo_a_pagar ;
+delete from conta_a_pagar ;
+
+delete from estoque_movimentacao ;
